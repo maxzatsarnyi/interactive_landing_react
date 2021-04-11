44eb8aaa6a91e0ec253ef1a82f5acf1daccb20d5
maxzatsanyi <zatsanyi.max@gmail.com>
2021-04-11 19:00:11 +0300

    Works

--- src/components/sections/Works/WorksElements.js
+++ src/components/sections/Works/WorksElements.js
@@ -1,45 +1,94 @@
 import styled from 'styled-components';
 
 export const WorksContainer = styled.div`
-    height: 798px;
     display: flex;
     align-items: center;
     justify-content: center;
+
 `;
 
 export const WorksWrapper = styled.div`
     width: 100%;
-    max-width: 980px;
-    height: 642px;
+    max-width: 62%;
     margin: 0 auto;
+    margin-top: 30px;
     display: flex;
     flex-direction: column;
-    /* align-items: center; */
-    
     justify-content: center;
+
+    @media screen and (max-width: 1400px){
+        max-width: 90%;
+    }
+    @media screen and (max-width: 768px){
+        margin-top: 0px;
+
+        flex-direction: column;
+        align-items: flex-start;
+    }
+    @media screen and (max-width: 480px){
+        max-width: 94%;
+        margin-top: 0px;
+    }
 `;
 
 export const WorksContent = styled.div`
-    margin-top: 30px;
+    margin: 30px auto 0 auto;
+    width: 100%;
+    max-width: 50%;
+
+    @media screen and (max-width: 768px){
+        max-width: 100%;
+        font-size: 16px;
+    }
+`;
+
+export const Slider = styled.div`
+    display: flex;
     
+    @media screen and (max-width: 768px){
+        flex-direction: column;
+        /* justify-content: flex-start; */
+    }
 `;
 
 export const WorksSliderCont = styled.div`
     display: flex;
-    justify-content: space-around;
-    /* width: 100%; */
-    max-width: 980px;   
+    flex-direction: column;
+    justify-content: space-between;
+    margin: 0 auto;
+    width: 84%;   
+
+    @media screen and (max-width: 768px){
+        width: 100%;    
+    }
 `;
 
 export const WorksMenu = styled.ul`
     list-style: none;   
     margin-left: 20px;
-    /* margin-right: 105px;  */
+    margin: 0 auto;
+    width: 100%;
+
+    @media screen and (max-width: 768px){
+        display: flex;
+        overflow-x: auto;
+    }
 `;
 
 export const WorksItem = styled.li`
     margin-top: 30px;
     max-width: 295px;
+    cursor: pointer;
+        
+    @media screen and (max-width: 768px){
+        width: 100%;
+        white-space: nowrap;
+        display: block;
+
+        ::-webkit-scrollbar {
+            display: none;
+        }
+    }
 `;
 
 export const ItemLine = styled.div`
@@ -56,7 +105,8 @@ export const ItemText = styled.p`
     line-height: 23px;
     font-weight: ${({isActive}) => (isActive === true ? 'bold' : 'normal')};
     color: ${({isActive}) => (isActive === true ? 'var(--primary-color)' : 'var(--text-third-color)')};
-    margin-left: 20px;
+    margin-right: 20px;
+
     ::before{
         content:'';
         display:inline-block;
@@ -69,12 +119,16 @@ export const ItemText = styled.p`
         height: ${({isActive}) => (isActive === true ? '2px' : '1px')};
         background: ${({isActive}) => (isActive === true ? 'var(--primary-color)' : 'var(--text-third-color)')};
     }
+
+    @media screen and (max-width: 768px){
+        min-width: 100%;
+        font-size: 16px;
+
+        ::before{
+            display:none;
+        }
+    }
 `;
-
-
-// export const WorksContent = styled.div`
-    
-// `;
 
 export const ContentTitle = styled.p`
     color: var(--text-third-color);
@@ -102,4 +156,30 @@ export const ContentNoticeP = styled.p`
     font-size: 16px;
     line-height: 22px;
     color: var(--text-third-color);
+`;
+
+export const StepCount = styled.div`
+    transition: 0.5s ease-in-out;
+    color: var(--text-first-color);
+    font-family: "Epilogue", sans-serif;
+    font-weight: bold;
+    font-size: 22px;
+    line-height: 23px;
+`;
+
+export const StepCountWrap = styled.div`
+    margin-top: 60px;
+`;
+
+export const StepCountH2 = styled.h2`
+    margin-top: 10px;
+    font-family: "Epilogue", sans-serif;
+    font-weight: bold;
+    font-size: 32px;
+    transition: 0.5s ease-in-out;
+    color: var(--text-first-color);
+    
+    @media screen and (max-width: 480px){
+        font-size: 22px;
+    }
 `;
--- src/components/sections/Works/index.js
+++ src/components/sections/Works/index.js
@@ -3,25 +3,73 @@ import {
     WorksContainer,
     WorksWrapper,
     WorksSliderCont,
-    WorksMenu,
-    WorksItem,
-    ItemLine,
-    ItemText,
     WorksContent,
     ContentTitle,
     ContentP,
     SpanP,
     ContentNotice,
     ContentNoticeP,
-
+    StepCount,
+    StepCountWrap,
+    StepCountH2,
+    Slider,
+    WorksItem,
+    WorksMenu,
+    ItemText,
+    
 } from './WorksElements';
 import Steps from '../../shared/Steps/index';
 import data from './Data';
+const stepsOptions = [
+    {
+        step: 1,
+        label: 'Step 1',
+        name: 'step 1',
+        line:  true,
+        isSelected:  true,
+        text: 'Dui accumsan sit',
+    },
+    {
+        step: 2,
+        label: 'Step 2',
+        name: 'step 2',
+        line:  true,
+        isSelected:  true,
+        text: 'Vitae proin sagittis',
 
+    },
+    {
+        step: 3,
+        label: 'Step 3',
+        name: 'step 3',
+        line:  true,
+        isSelected:  true,
+        text: 'Sed ut perspiciatis',
+
+    },
+    {
+        step: 4,
+        label: 'Step 3',
+        name: 'step 3',
+        line:  false,
+        isSelected:  false,
+        text: 'Leo integer malesuada',
+
+    },
+  ];
+
+const textSlide = [
+    `Leo integer malesuada`,
+    `Leo integer malesuada`,
+    `Leo integer malesuada`,
+]
 const Works = () => {
     const [currentSlide, setCurrentSlide] = useState(2);
     const [currentObj, setCurrentObj] = useState(data[currentSlide-1]);
 
+    const [stepsOpt, setStepsOpt] = useState(stepsOptions);
+    const [currentStep, setCurrentStep] = useState(3);
+    
     const changeSlide = ({currentTarget}) => {
         setCurrentSlide(currentTarget.dataset.slide)
         setCurrentObj(data[currentTarget.dataset.slide-1])
@@ -34,40 +82,52 @@ const Works = () => {
         <>
             <WorksContainer>
                 <WorksWrapper>
-                    <Steps/>
+                    <Steps 
+                        stepsOpt={stepsOpt} 
+                        currentStep={currentStep} 
+                        setStepsOpt={setStepsOpt}
+                        setCurrentStep={setCurrentStep}
+                    />
                     <WorksSliderCont>
-                        <WorksMenu>
-                            <WorksItem data-slide={1} onClick={changeSlide}>
-                                <ItemText isActive={currentSlide == 1 && true}>Nemo enim</ItemText>
-                            </WorksItem>
-                            <WorksItem data-slide={2} onClick={changeSlide}>
-                                <ItemText isActive={currentSlide == 2 && true}>Die ipsam voluptatem</ItemText>
-                            </WorksItem>
-                            <WorksItem data-slide={3} onClick={changeSlide}>
-                                <ItemText isActive={currentSlide == 3 && true}>Ut enim ad minima</ItemText>
-                            </WorksItem>
-                            <WorksItem data-slide={4} onClick={changeSlide}>
-                                <ItemText isActive={currentSlide == 4 && true}>Quis autem vel</ItemText>
-                            </WorksItem>
-                            <WorksItem data-slide={5} onClick={changeSlide}>
-                                <ItemText isActive={currentSlide == 5 && true}>Sed ut perspiciatis</ItemText>
-                            </WorksItem>
-                        </WorksMenu>
+                        <StepCountWrap>
+                            <StepCount>Step {currentStep}</StepCount>
+                            <StepCountH2>Sed ut perspiciatis</StepCountH2>
+                        </StepCountWrap>
+                        <Slider>
+                            <WorksMenu>
+                                <WorksItem data-slide={1} onClick={changeSlide}>
+                                    <ItemText isActive={currentSlide == 1 && true}>Nemo enim</ItemText>
+                                </WorksItem>
+                                <WorksItem data-slide={2} onClick={changeSlide}>
+                                    <ItemText isActive={currentSlide == 2 && true}>Die ipsam voluptatem</ItemText>
+                                </WorksItem>
+                                <WorksItem data-slide={3} onClick={changeSlide}>
+                                    <ItemText isActive={currentSlide == 3 && true}>Ut enim ad minima</ItemText>
+                                </WorksItem>
+                                <WorksItem data-slide={4} onClick={changeSlide}>
+                                    <ItemText isActive={currentSlide == 4 && true}>Quis autem vel</ItemText>
+                                </WorksItem>
+                                <WorksItem data-slide={5} onClick={changeSlide}>
+                                    <ItemText isActive={currentSlide == 5 && true}>Sed ut perspiciatis</ItemText>
+                                </WorksItem>
+                            </WorksMenu>
 
-                        <WorksContent>
-                            {currentObj && (
-                                <>
-                                <ContentTitle>{currentObj.title}</ContentTitle>
-                                {currentObj.textOfpParagraphs.map(({id, text}) => (
-                                    <ContentP key={id}><SpanP>{id}.</SpanP> {text}</ContentP>
-                                ))}
-                                <ContentNotice>
-                                    <SpanP>Notice:</SpanP>
-                                    <ContentNoticeP>{currentObj.notice}</ContentNoticeP>
-                                </ContentNotice>
-                            </>
-                            )}
-                        </WorksContent>
+                            <WorksContent>
+                                {currentObj && (
+                                    <>
+                                    <ContentTitle>{currentObj.title}</ContentTitle>
+                                    {currentObj.textOfpParagraphs.map(({id, text}) => (
+                                        <ContentP key={id}><SpanP>{id}.</SpanP> {text}</ContentP>
+                                    ))}
+                                    <ContentNotice>
+                                        <SpanP>Notice:</SpanP>
+                                        <ContentNoticeP>{currentObj.notice}</ContentNoticeP>
+                                    </ContentNotice>
+                                </>
+                                )}
+                            </WorksContent>
+                        </Slider>
+                        
                     </WorksSliderCont>
                 </WorksWrapper>
             </WorksContainer>
