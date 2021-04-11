2c960e0147a612ca1c2984d5a07e55101371213d
maxzatsanyi <zatsanyi.max@gmail.com>
2021-04-11 19:01:42 +0300

    Main file

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from '../../components/shared/Navbar/index';
 import PromoSection from '../../components/sections/Promo/index';
 import ClockSection from '../../components/sections/Clock/index';
@@ -8,20 +8,35 @@ import Notebook from '../../components/sections/Notebook';
 import Works from '../../components/sections/Works/index';
 import Pin from '../../components/sections/Pin/index';
 import Faq from '../../components/sections/Faq/index';
-import Footer from '../../components/Footer/index';
+import Footer from '../../components/sections/Footer/index';
+import Reviews from '../../components/sections/Reviews/index';
+import Modal from '../../components/shared/Modal/index'
+import Sidebar from '../../components/shared/Sidebar/index'
+
 
 const Home = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+
+    const toggleModal = () => {
+        setShowModal(prevState => !prevState)
+    };
+
+    const toggleBurger = () => setIsOpen(prevState => !prevState);
     return (
         <>
-            {/* <Navbar /> */}
-            {/* <PromoSection /> */}
-            {/* <ClockSection /> */}
-            {/* <Calculator /> */}
-            {/* <Earpods /> */}
-            {/* <Notebook /> */}
-            {/* <Works /> */}
-            {/* <Pin /> */}
-            {/* <Faq /> */}
+            <Navbar toggleBurger={toggleBurger} toggleModal={toggleModal}/>
+            <Sidebar isOpen={isOpen} toggleBurger={toggleBurger}/>
+            <Modal showModal={showModal} toggleModal={toggleModal}/>
+            <PromoSection/>
+            <ClockSection />
+            <Calculator />
+            <Works />
+            <Reviews />
+            <Pin />
+            <Earpods />
+            <Notebook />
+            <Faq />
             <Footer />
         </>
     )
